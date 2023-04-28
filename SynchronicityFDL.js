8baf3502d174228f3b0aef8fc21eb20d7eb6037c
Marc Hage <tramway.luckier_07@icloud.com>
2023-04-29 01:37:49 +0200

    static stuff

--- SynchronicityFDL.js
+++ SynchronicityFDL.js
@@ -1,28 +1,22 @@
 // ==UserScript==
 // @name         SynchronicityFDL
 // @description  Synchronized blob fetching 5 at a time, something with synchronized blob downloading with mutex (Taken from StackOverflow, ...) (synchronicity starts here, by not awaiting async function)
-// @version      0.1.1
+// @version      0.2.0
 // @author       marchage
 // @match        *://*
 // @require      https://raw.githubusercontent.com/marchage/lib-user.js/main/Semaphore.js
 // ==/UserScript==
 /* eslint-env greasemonkey */
 class SynchronicityFDL {
-    #semaphore
-    #mutex
-    #delay
+    static #semaphore = new Semaphore(5)
+    static #mutex = new Semaphore(1)
+    static #delay = 100
 
-    constructor(concurrency = 5) {
-        this.#semaphore = new Semaphore(concurrency)
-        this.#mutex = new Semaphore(1)
-        this.#delay = 100
-    }
-
-    #sleep(ms) {
+    static #sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms))
     }
 
-    #downloadBlob(blob, name) {
+    static #downloadBlob(blob, name) {
         const anchor = document.createElement("a")
         anchor.setAttribute("download", name || "")
         anchor.href = URL.createObjectURL(blob)
@@ -30,28 +24,26 @@ class SynchronicityFDL {
         setTimeout(_ => URL.revokeObjectURL(blob), 30000)
     }
 
-    async #fetchBlob(url) {
-        if (url == null) return
-
+    static async #fetchBlob(url) {
+        // if (url == null) return
         const res = await fetch(url).then(res => { if (!res.ok) throw new Error("Not 2xx response", { cause: res }); else return res })
         const resBuf = await res.arrayBuffer()
-        const resBlob = new Blob([resBuf], { type: 'application/octet-stream' })
-
-        return resBlob
-    }
-    
-    async fetchBlobSynced (url) {
-        await this.#semaphore.acquire()
-        const blob = await this.#fetchBlob(url)
-        this.#semaphore.release()
+        const blob = new Blob([resBuf], { type: 'application/octet-stream' })
         return blob
     }
 
-    async downloadBlobSynced (blob, name) {
-        await this.#mutex.acquire()
-        this.#downloadBlob(blob, name)
-        await this.#sleep(this.#delay)
-        this.#mutex.release()
+    static async downloadBlobSynced(blob, name) {
+        await SynchronicityFDL.#mutex.acquire()
+        SynchronicityFDL.#downloadBlob(blob, name)
+        await SynchronicityFDL.#sleep(this.#delay)
+        SynchronicityFDL.#mutex.release()
         console.info("Downloaded", name)
+    }
+
+    static async fetchBlobSynced(url) {
+        await SynchronicityFDL.#semaphore.acquire()
+        const blob = await SynchronicityFDL.#fetchBlob(url)
+        SynchronicityFDL.#semaphore.release()
+        return blob
     }
 }
