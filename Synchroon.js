4dcb38c6c1e8da77c322cc5764fa8dc766c21acf
Marc Hage <tramway.luckier_07@icloud.com>
2023-04-30 18:01:25 +0200

    Changes semaphore implementations again, back to what it was.

--- Synchroon.js
+++ Synchroon.js
@@ -1,133 +1,90 @@
 // ==UserScript==
 // @name         Synchroon
-// @description  Async number semaphore guarded synchronized binary downloading - 5 at a time. Taken from StackOverflow 
-//               first, and later https://alexn.org/blog/2020/04/21/javascript-semaphore/. Synchronicity starts 
-//               here, by not awaiting public async functions, but rather awaiting the private ones (as this script does).
-// @version      0.2.1
+// @description  Async number semaphore guarded synchronized binary downloading - 5 at a time. Semaphore Taken from SO and adapted. 
+//               Second semaphore implementation based on https://alexn.org/blog/2020/04/21/javascript-semaphore/. 
+//               Synchronicity starts here, by not awaiting public async functions, but rather awaiting the private ones.
+// @version      0.2.2
 // @author       marchage
 // @match        *://*
 // ==/UserScript==
 /* eslint-env greasemonkey */
 
-class AsyncSemaphore {
-    #available
-    #upcoming
-    #heads
+/**
+ * Async counting semaphore functionality, based on Edsger Dijkstra's concept from 
+ * the '60s, using JS Promises. Taken from StackOverflow and adapted. General workings 
+ * are a synchronization primitive used to control access to a common resource by 
+ * multiple threads and avoid critical section problems in a concurrent system.
+ * 
+ * @simple Synchronously do some shared thing - like downloading items on a 
+ *         list - 5 at the same time. More than that is queued for later when 
+ *         another finishes.
+ * @date 4/30/2023 - 2:17:22 AM
+ *
+ * @class Semaphore
+ * @typedef {Semaphore}
+ */
+class Semaphore {
+    /** (ficticious) semaphore #S = #max - #count */
+    /** @type {number} */
+    #max
+    /** @type {number} */
+    #count
+    /** @type {Array<Function>} */
+    #queue
 
-    #completeFn = () => {/* */ }
-    #completePr = () => new Promise()
-
-    
     /**
-     * Creates an instance of AsyncSemaphore.
-     *
-     * @constructor
-     * @param {number} [workersCount=5] Number of workers to allow to run at the same time.
-     */
-    constructor(workersCount = 5) {
-        if (workersCount <= 0) throw new Error("workersCount must be positive")
-        this.#available = workersCount
-        this.#upcoming = []
-        this.#heads = []
-        this.#refreshComplete()
-    }
-
-    // Private API
-    async #execWithRelease(f = async () => Promise) {
-        try {
-            return await f()
-        } finally {
-            this.#release()
-        }
-    }
-
-    #queue() {
-        if (!this.#heads.length) {
-            this.#heads = this.#upcoming.reverse()
-            this.#upcoming = []
-        }
-        return this.#heads
-    }
-
-    #acquire() {
-        if (this.#available > 0) {
-            this.#available -= 1
-            return undefined
-        } else {
-            let fn = new Function('() => {}')
-            const p = new Promise(ref => { fn = ref })
-            this.#upcoming.push(fn)
-            return p
-        }
-    }
-
-    #release() {
-        const queue = this.#queue()
-        if (queue.length) {
-            const fn = queue.pop()
-            if (fn) fn()
-        } else {
-            this.#available += 1
-
-            if (this.#available >= this.workersCount) {
-                const fn = this.#completeFn
-                this.#refreshComplete()
-                fn()
-            }
-        }
-    }
-
-    #refreshComplete() {
-        let fn = () => () => {/***/ }
-        this.#completePr = new Promise(r => { fn = r })
-        this.#completeFn = fn
-    }
-
-    // Public API
-    /**
-     * Acquire a lock and execute the given function. The lock is released when 
-     * the function returns.
-     *
-     * @async
-     * @param {() => any} [f=() => Promise]
-     * @returns {any) => unknown}
-     */
-    async withLock(f = () => Promise) {
-        await this.#acquire()
-        return this.#execWithRelease(f)
-    }
-
-    
-    /**
-     * Acquire a lock and execute the given function. The lock is not released 
-     * when the function returns, but rather some time in the future.
-     *
-     * @async
-     * @param {() => any} [f=() => Promise]
-     * @returns {any) => any}
-     */
-    async withLockRunAndForget(f = () => Promise) {
-        await this.#acquire()
-        // Ignoring returned promise on purpose!
-        this.#execWithRelease(f)
-    }
-
-    
-    /**
-     * Wait for all locks to be released.
-     * @date 4/30/2023 - 11:20:51 AM
+     * Creates an instance of an async counting semaphore for concurrency management.
+     * @date 4/30/2023 - 2:42:38 AM
      * @author marchage
      *
-     * @async
-     * @returns {unknown}
+     * @constructor
+     * @param {number} [max=1] - Maximum number of concurrent operations
      */
-    async awaitTerminate() {
-        if (this.#available < this.workersCount) {
-            return this.#completePr
+    constructor(max = 1) {
+        if (max < 1) { max = 1 }
+        this.#max = max
+        this.#count = 0
+        this.#queue = []
+    }
+
+    /**
+     * Commen in literature P-function, decrementing semaphore #S by 1. When #S 
+     * is negative the caller is blocked i.e. added to semaphore's queue and 
+     * resolving is posponed.
+     * @date 4/30/2023 - 2:46:47 AM
+     * @author marchage
+     *
+     * @returns {*}
+     */
+    acquire() {
+        let promise
+        if (this.#count < this.#max) {
+            promise = Promise.resolve()
+        } else {
+            promise = new Promise(resolve => {
+                this.#queue.push(resolve)
+            })
         }
+        this.#count++
+        return promise
+    }
+
+
+    /**
+     * Commen in literature V-function, incrementing semaphore #S by 1, representing 
+     * an access slot of total max concurrent that has become available. If there
+     * are any waiting in the queue, the first one is resolved.
+     * @date 4/30/2023 - 2:47:41 AM
+     * @author marchage
+     */
+    release() {
+        if (this.#queue.length > 0) {
+            const resolve = this.#queue.shift()
+            resolve()
+        }
+        this.#count--
     }
 }
-
 
 /**
  * Synchronized blob fetching 5 at a time, something with synchronized blob downloading with mutex (Taken from StackOverflow, ...)
@@ -139,7 +96,7 @@ class AsyncSemaphore {
  */
 class Synchroon {
     /** @type {AsyncSemaphore} */
-    static #semaphore = new AsyncSemaphore(5)
+    static #semaphore = new AsyncSemaphore(6)
     /** @type {AsyncSemaphore} semaphore of 1, behavous like a simple mutex-lock */
     static #mutex = new AsyncSemaphore(1)
     /** @type {number} Don't know why this was, but it was needed for some reason. Hopefully not only demonstration purpouses?! */
@@ -194,12 +151,11 @@ class Synchroon {
      * @returns {*}
      */
     static async downloadBlobSynced(blob, name) {
-        Synchroon.#mutex.withLockRunAndForget(async () => {
-            Synchroon.#downloadBlob(blob, name)
-            await Synchroon.#sleep(Synchroon.#delay)
-            // Synchroon.#mutex.release()
-            console.info("Downloaded", name)
-        })
+        await Synchroon.#mutex.acquire()
+        Synchroon.#downloadBlob(blob, name)
+        await Synchroon.#sleep(Synchroon.#delay)
+        Synchroon.#mutex.release()
+        console.info("Downloaded", name)
     }
 
     /**
@@ -212,10 +168,9 @@ class Synchroon {
      * @returns {unknown}
      */
     static async fetchBlobSynced(url) {
-        await Synchroon.#semaphore.withLock(async () => {
-            const blob = await Synchroon.#fetchBlob(url)
-            // Synchroon.#semaphore.release()
-            return blob
-        })
+        await Synchroon.#semaphore.acquire()
+        const blob = await Synchroon.#fetchBlob(url)
+        Synchroon.#semaphore.release()
+        return blob
     }
 }
